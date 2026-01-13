9f9057095251fbb25b6df8da62650c2beb47ef91
Rayane Leandro de Lima Nogueira <rayaneleandrodelima2016@gmail.com>
2026-01-13 15:25:40 -0300

    Projeto Blog Pessoal com NestJS

---
+++ src/postagem/controllers/postagem.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Postagem } from '../entities/postagem.entity';
+import { PostagemService } from '../services/postagem.service';
+import { DeleteResult } from 'typeorm';
+
+@Controller('/postagens')
+export class PostagemController {
+  constructor(private readonly postagemService: PostagemService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Postagem[]> {
+    return this.postagemService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.create(postagem);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.updateDateColumn(postagem);
+  }
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return this.postagemService.delete(id);
+  }
+}
